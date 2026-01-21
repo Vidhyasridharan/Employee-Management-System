d1b3adf3332cc69963d578986df7570a447f916c
Vidhyasridharan <107660121+Vidhyasridharan@users.noreply.github.com>
2026-01-20 22:54:33 -0800

    Update EmployeeEntity.java

--- src/main/java/com/example/demo/test/entity/EmployeeEntity.java
+++ src/main/java/com/example/demo/test/entity/EmployeeEntity.java
@@ -1,57 +1,70 @@
 package com.example.demo.test.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
-@Table(name = "Employees")
+@Table(name = "employees")
 public class EmployeeEntity {
-	public EmployeeEntity() {
-		
-	}
-public EmployeeEntity(String firstName, String lastName, String email) {
-		super();
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-	}
 
+    public EmployeeEntity() {
+    }
 
-public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)	
-private Long id;
-@Column(name="firstName" )
-private String firstName;
-@Column(name="lastName" )
-private String lastName;
-@Column(name="email" )
-private String email;
+    public EmployeeEntity(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "First name must not be empty")
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @NotBlank(message = "Last name must not be empty")
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email must not be empty")
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
